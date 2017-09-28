8975de007db4bb57aaafcab4cca64f408deaa313
omelchenko <dok.ttv@gmail.com>
2017-09-28 11:13:23 +0300

    Method "sort" added (Practice 02 p. 3-4)

--- src/com/netcracker2017/Main.java
+++ src/com/netcracker2017/Main.java
@@ -1,15 +1,32 @@
 package com.netcracker2017;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        print(new String[]{"sdf", "test", "another one"});
+        String[] stringArray = new String[]{"zb", "yb", "za", "fg", "fa", "az"};
+        sort(stringArray);
+        print(stringArray);
     }
 
-    public static void print(String[] args){
-        for(String s : args){
+    public static void print(String[] args) {
+        for (String s : args) {
             System.out.println(s);
+        }
+    }
+
+    public static void sort(String[] args) {
+        // or just Arrays.sort(args);
+        for (int i = 0; i < args.length; i++) {
+            for (int j = 0; j < args.length - i - 1; j++) {
+                if ((args[j].compareTo(args[j + 1])) > 0) {
+                    String temp = args[j];
+                    args[j] = args[j + 1];
+                    args[j + 1] = temp;
+                }
+            }
         }
     }
 }
