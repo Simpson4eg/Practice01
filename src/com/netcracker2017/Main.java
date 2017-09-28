bb2b172b28d26996868b435f0cb27479e2c58a88
omelchenko <dok.ttv@gmail.com>
2017-09-28 10:47:52 +0300

    Method "print" added (Practice 02 p. 1-2)

--- src/com/netcracker2017/Main.java
+++ src/com/netcracker2017/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        print(new String[]{"sdf", "test", "another one"});
+    }
+
+    public static void print(String[] args){
+        for(String s : args){
+            System.out.println(s);
+        }
     }
 }
